537f8c84652b04729b7b1c26d32d72ed8c234017
Palafox003 <israelpalafox@utgz.edu.mx>
2017-01-11 16:17:52 -0600

    Ejercicoio2
    Ejercicio en clase 11/01/17

---
+++ Conectar.php
@@ -0,0 +1,5 @@
+<?php  
+require_once"conDB.php";
+
+	$con=new Conectar();
+?>
---
+++ conDB.php
@@ -0,0 +1,27 @@
+<?php 
+class Conectar
+{
+	private $user="root";
+	private $db="Ejercicio2";
+	private $host="localhost";
+	private $pass="";
+	private $con;
+	
+	function __construct()
+	{
+		$this->conexion();
+	}
+
+	private function conexion(){
+		$con2=new mysqli($this->host,$this->user,$this->pass,$this->db);
+			if(mysqli_connect_errno()){
+				echo "Error de conexion.";
+			}else{
+				echo "Conectado.";
+				$this->con=$con2;
+			}
+
+	}
+
+}
+?>
